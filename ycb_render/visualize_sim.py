98b2828143ebf76b65cb640a83180611cebc003a
Dingkun Guo <code@guodk.com>
2023-01-25 16:00:33 -0500

    wip

--- ycb_render/visualize_sim.py
+++ ycb_render/visualize_sim.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 
     renderer.set_fov(60)
     renderer.set_light_pos([0, 0, 0])
-    renderer.set_camera([0, 0, 0], [1, 0, 0], [0, 0, 1])
+    renderer._set_camera_view([0, 0, 0], [1, 0, 0], [0, 0, 1])
 
     image_tensor = torch.cuda.FloatTensor(height, width, 4).detach()
     seg_tensor = torch.cuda.FloatTensor(height, width, 4).detach()
